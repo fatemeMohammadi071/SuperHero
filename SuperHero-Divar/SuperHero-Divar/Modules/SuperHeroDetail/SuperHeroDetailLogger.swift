cf5b133b73169cd50f46598e7a0e2172f2792622
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2021-12-27 23:03:52 +0330

    Added SuperHeroDetail cell

---
+++ SuperHero-Divar/SuperHero-Divar/Modules/SuperHeroDetail/SuperHeroDetailLogger.swift
@@ -0,0 +1,15 @@
+//
+//  SuperHeroDetailLogger.swift
+//  SuperHero
+//
+//  Created by Fateme on 10/4/1400 AP.
+//  Copyright (c) 1400 AP ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+class SuperHeroDetailLogger: Logger {
+    override class var prefix: String {
+        return "SuperHeroDetail"
+    }
+}
