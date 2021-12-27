d5e78c04e4fe2b1505ebe767ab71f16a03fff241
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2021-12-27 22:20:10 +0330

    Added default section header

---
+++ SuperHero-Divar/SuperHero-Divar/Common/TableView/DefaultSectionHeaderView/DefaultSectionHeaderFooterView.swift
@@ -0,0 +1,22 @@
+
+//
+//  DefaultSectionHeaderFooterView.swift
+//  Boatingo
+//
+//  Created by Ali Motevali on 4/7/20.
+//  Copyright © 2020 RoundTableApps. All rights reserved.
+//
+
+import UIKit
+
+class DefaultSectionHeaderFooterView: UITableViewHeaderFooterView {
+    @IBOutlet private weak var bgView: UIView! {
+        didSet {
+            self.bgView.backgroundColor = .gray
+        }
+    }
+
+    func setBackgroundColor(_ color: UIColor) {
+        self.bgView.backgroundColor = color
+    }
+}
