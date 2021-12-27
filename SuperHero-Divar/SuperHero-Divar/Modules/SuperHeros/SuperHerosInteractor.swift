e18bac9ce8e437dbbca1330d8ff183cccadf707d
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2021-12-27 22:33:33 +0330

    Added interactor of superHero list

---
+++ SuperHero-Divar/SuperHero-Divar/Modules/SuperHeros/SuperHerosInteractor.swift
@@ -0,0 +1,83 @@
+//
+//  SuperHerosInteractor.swift
+//  SuperHero
+//
+//  Created by Fateme on 10/2/1400 AP.
+//  Copyright (c) 1400 AP ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+protocol SuperHerosBusinessLogic {
+    func fetchSuperHeros(request: SuperHeros.SuperHeros.Request)
+    func searchCharacter(request: SuperHeros.Search.Request)
+}
+
+protocol SuperHerosDataStore {}
+
+class SuperHerosInteractor: SuperHerosDataStore {
+    // MARK: - Object lifecycle
+    init() {
+        SuperHerosLogger.logInit(owner: String(describing: SuperHerosInteractor.self))
+    }
+    
+    // MARK: - Deinit
+    deinit {
+        SuperHerosLogger.logDeinit(owner: String(describing: SuperHerosInteractor.self))
+    }
+    
+    // MARK: - Properties
+    private let debouncer = Debouncer(timeInterval: 5)
+    
+    // MARK: Public
+    var presenter: SuperHerosPresentationLogic?
+    var worker: SuperHerosWorkerLogic?
+}
+
+// MARK: - Methods
+
+// MARK: Private
+private extension SuperHerosInteractor {}
+
+// MARK: Public
+extension SuperHerosInteractor {}
+
+// MARK: - Business Logics
+extension SuperHerosInteractor: SuperHerosBusinessLogic {
+    func fetchSuperHeros(request: SuperHeros.SuperHeros.Request) {
+        defer { presenter?.hideLoading(response: SuperHeros.Loading.Response()) }
+        presenter?.presentLoading(response: SuperHeros.Loading.Response())
+        worker?.getSuperHeros(offset: request.offset) { [weak self] (result) in
+            guard let self = self else { return }
+            switch result {
+            case .success(let superHerosInfo):
+                guard let superHeroInfos: [SuperHeroInfo] = superHerosInfo, !superHeroInfos.isEmpty else {
+                    self.presenter?.presentError(response: SuperHeros.ErrorModel.Response(requestError: SuperHeroErrors.errorNoResult))
+                    return
+                }
+                self.presenter?.presentSuperHeros(response: SuperHeros.SuperHeros.Response(superHeros: superHeroInfos))
+            case .failure(let erro):
+                self.presenter?.presentError(response: SuperHeros.ErrorModel.Response(requestError: erro))
+            }
+        }
+    }
+    
+    func searchCharacter(request: SuperHeros.Search.Request) {
+        debouncer.renewInterval()
+        debouncer.handler = { [weak self] in
+            self?.worker?.searchCharacter(name: request.name) { [weak self] (result) in
+                guard let self = self else { return }
+                switch result {
+                case .success(let superHerosInfo):
+                    guard let superHeroInfos: [SuperHeroInfo] = superHerosInfo, !superHeroInfos.isEmpty else {
+                        self.presenter?.presentError(response: SuperHeros.ErrorModel.Response(requestError: SuperHeroErrors.errorNoResult))
+                        return
+                    }
+                    self.presenter?.presentSearchResult(response: SuperHeros.SuperHeros.Response(superHeros: superHeroInfos))
+                case .failure(let error):
+                    self.presenter?.presentError(response: SuperHeros.ErrorModel.Response(requestError: error))
+                }
+            }
+        }
+    }
+}
