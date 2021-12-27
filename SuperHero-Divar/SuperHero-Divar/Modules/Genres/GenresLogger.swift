288a86549c83d8fe92c4f4f0cc33e9a83e1f6c1d
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2021-12-27 23:13:36 +0330

    Added logger of genres module

---
+++ SuperHero-Divar/SuperHero-Divar/Modules/Genres/GenresLogger.swift
@@ -0,0 +1,15 @@
+//
+//  GenresLogger.swift
+//  SuperHero
+//
+//  Created by Fateme on 12/27/21.
+//  Copyright (c) 2021 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+class GenresLogger: Logger {
+    override class var prefix: String {
+        return "Genres"
+    }
+}
