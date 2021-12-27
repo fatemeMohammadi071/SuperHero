596b6bea3c94ec8ea2818356f3bd362650fe090c
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2021-12-27 22:14:03 +0330

    Added enumeration of errors

---
+++ SuperHero-Divar/SuperHero-Divar/Common/SuperHeroErrors.swift
@@ -0,0 +1,14 @@
+//
+//  SuperHeroErrors.swift
+//  SuperHero
+//
+//  Created by Fateme on 10/2/1400 AP.
+//
+
+import Foundation
+
+import Foundation
+enum SuperHeroErrors: String, Error {
+    case errorOnParsing = "An error occured while parsing the data"
+    case errorNoResult = "There is no result"
+}
