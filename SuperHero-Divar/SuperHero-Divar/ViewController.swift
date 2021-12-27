be67e3c0576080c781b3e02a82253b4a95526ad1
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2021-12-27 21:57:14 +0330

    Initialized

---
+++ SuperHero-Divar/SuperHero-Divar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperHero-Divar
+//
+//  Created by Fateme on 10/6/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
