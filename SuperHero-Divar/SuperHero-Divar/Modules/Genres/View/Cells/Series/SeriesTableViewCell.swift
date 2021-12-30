17d37729b1593f4eef6fbbfac4569ba94df100b8
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2021-12-31 00:59:27 +0330

    Create UI of series

--- SuperHero-Divar/SuperHero-Divar/Modules/Genres/View/Cells/Series/SeriesTableViewCell.swift
+++ SuperHero-Divar/SuperHero-Divar/Modules/Genres/View/Cells/Series/SeriesTableViewCell.swift
@@ -19,9 +19,11 @@ class SeriesTableViewCell: UITableViewCell {
     
     // MARK: - Outlets
     @IBOutlet private weak var typeLabel: UILabel!
-    @IBOutlet private weak var languageLabel: UILabel!
-    @IBOutlet private weak var priceLabel: UILabel!
-
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var startTitleLabel: UILabel!
+    @IBOutlet private weak var startLabel: UILabel!
+    @IBOutlet private weak var endTitleLabel: UILabel!
+    @IBOutlet private weak var endLabel: UILabel!
 }
 // MARK: - Binder
 extension SeriesTableViewCell: Binder {
@@ -37,6 +39,33 @@ extension SeriesTableViewCell: Binder {
 // MARK: Private
 private extension SeriesTableViewCell {
     private func setupViews(model: SeriesDetail) {
-        // TODO: Set text and color
+        self.setupTitleLabel(text: model.title ?? "")
+        self.setupTypeLabel(text: model.type ?? "No type")
+        let startDate = model.startYear
+        let endDate = model.endYear
+        self.setupStartLabel(text: "\(String(describing: startDate))")
+        self.setupEndLabel(text: "\(String(describing: endDate))")
+    }
+    
+    func setupTitleLabel(text: String) {
+        self.titleLabel.text = text
+        self.titleLabel.font = UIFont(type: .montserratBlack)
+    }
+    
+    func setupTypeLabel(text: String) {
+        self.typeLabel.text = "Type: " + text
+        self.typeLabel.font = UIFont(type: .montserratLight)
+    }
+    
+    func setupStartLabel(text: String) {
+        self.startLabel.text = text
+        self.startLabel.font = UIFont(type: .montserratLight)
+        self.startTitleLabel.font = UIFont(type: .montserratSemiBold)
+    }
+    
+    func setupEndLabel(text: String) {
+        self.endLabel.text = text
+        self.endLabel.font = UIFont(type: .montserratLight)
+        self.endTitleLabel.font = UIFont(type: .montserratSemiBold)
     }
 }
